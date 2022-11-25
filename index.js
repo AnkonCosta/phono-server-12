912003eac3d738707fa804aa677a96418bc0c26b
AnkonCosta <ankoncosta247@gmail.com>
2022-11-25 16:00:15 +0600

    added query

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -19,6 +20,22 @@ const client = new MongoClient(uri, {
 });
 console.log(uri);
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("unauthorized access");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 async function run() {
   try {
     const phoneCollection = client
@@ -60,12 +77,30 @@ async function run() {
       const result = await bookingCollection.insertOne(booking);
       res.send(result);
     });
-    // get bookings
-    app.get("/bookings", async (req, res) => {
+    app.get("/bookings", verifyJWT, async (req, res) => {
       const email = req.query.email;
+      const decodedEmail = req.decoded.email;
+
+      if (email !== decodedEmail) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+
       const query = { email: email };
       const bookings = await bookingCollection.find(query).toArray();
-      res.send(bookings)
+      res.send(bookings);
+    });
+    // jwt
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await userColletcion.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "1h",
+        });
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: "" });
     });
 
     app.post("/users", async (req, res) => {
@@ -73,6 +108,13 @@ async function run() {
       users.time = Date();
       const result = await userColletcion.insertOne(users);
       res.send(result);
+    });
+
+    // get all users
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const users = await userColletcion.find(query).toArray();
+      res.send(users);
     });
   } finally {
   }
